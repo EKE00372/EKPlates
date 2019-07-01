1a3282438efe62f3c846d6b438595798eb54289a
EKE00372 <eke00372@gmail.com>
2019-07-01 11:41:36 +0800

    R2.1 Fix bar style mouseover highlight error Fix memory leak (Credits Rubgrsch). Hide level text when unit level is same as player if player got max level.

--- EKPlates/EKPlates.lua
+++ EKPlates/EKPlates.lua
@@ -148,8 +148,8 @@ local function CreateAuraIcon(parent)
 	button:SetSize(C.auraiconsize, C.auraiconsize)
 
 	button.icon = button:CreateTexture(nil, "OVERLAY", nil, 3)
-	button.icon:SetPoint("TOPLEFT",button,"TOPLEFT", 1, -1)
-	button.icon:SetPoint("BOTTOMRIGHT",button,"BOTTOMRIGHT",-1, 1)
+	button.icon:SetPoint("TOPLEFT", button,"TOPLEFT", 1, -1)
+	button.icon:SetPoint("BOTTOMRIGHT", button,"BOTTOMRIGHT",-1, 1)
 	button.icon:SetTexCoord(.08, .92, 0.08, 0.92)
 	
 	button.overlay = button:CreateTexture(nil, "ARTWORK", nil, 7)
@@ -159,8 +159,8 @@ local function CreateAuraIcon(parent)
 	button.bd = button:CreateTexture(nil, "ARTWORK", nil, 6)
 	button.bd:SetTexture("Interface\\Buttons\\WHITE8x8")
 	button.bd:SetVertexColor(0, 0, 0)
-	button.bd:SetPoint("TOPLEFT",button,"TOPLEFT", -1, 1)
-	button.bd:SetPoint("BOTTOMRIGHT",button,"BOTTOMRIGHT", 1, -1)
+	button.bd:SetPoint("TOPLEFT", button,"TOPLEFT", -1, 1)
+	button.bd:SetPoint("BOTTOMRIGHT", button,"BOTTOMRIGHT", 1, -1)
 	
 	button.text = createnumber(button, "OVERLAY", G.aurafontsize, G.fontflag, "CENTER")
 	button.text:SetPoint("BOTTOM", button, "BOTTOM", 0, -2)
@@ -174,6 +174,22 @@ local function CreateAuraIcon(parent)
 end
 
 -- Creat aura icon and update / 更新圖示
+local function AuraIconOnUpdate(self, elapsed)
+	if not self.duration then return end
+	
+	self.elapsed = (self.elapsed or 0) + elapsed
+
+	if self.elapsed < .2 then return end
+	self.elapsed = 0
+
+	local timeLeft = self.expirationTime - GetTime()
+	if timeLeft <= 0 then
+		self.text:SetText(nil)
+	else
+		self.text:SetText(FormatTime(timeLeft))
+	end
+end
+
 local function UpdateAuraIcon(button, unit, index, filter)
 	local name, icon, count, debuffType, duration, expirationTime, _, _, _, spellID = UnitAura(unit, index, filter)
 
@@ -197,21 +213,7 @@ local function UpdateAuraIcon(button, unit, index, filter)
 		button.count:SetText(count)
 	end
 	]]--
-	button:SetScript("OnUpdate", function(self, elapsed)
-		if not self.duration then return end
-		
-		self.elapsed = (self.elapsed or 0) + elapsed
-
-		if self.elapsed < .2 then return end
-		self.elapsed = 0
-
-		local timeLeft = self.expirationTime - GetTime()
-		if timeLeft <= 0 then
-			self.text:SetText(nil)
-		else
-			self.text:SetText(FormatTime(timeLeft))
-		end
-	end)
+	button:SetScript("OnUpdate", AuraIconOnUpdate)
 	
 	button:Show()
 end
@@ -432,6 +434,15 @@ if C.classresource_show then
 		end
 
 	end
+	
+	local function RuneOnUpdate(self, elapsed)
+		self.duration = self.duration + elapsed
+		if self.duration >= self.max or self.duration <= 0 then
+			self.value:SetText("")
+		else
+			self.value:SetText(FormatTime(self.max - self.duration))
+		end
+	end
 
 	Resourcebar:SetScript("OnEvent", function(self, event, unit, powerType)
 		if event == "PLAYER_TALENT_UPDATE" then
@@ -550,14 +561,7 @@ if C.classresource_show then
 				self[rid].tex:SetColorTexture(.3, .3, .3)
 				self[rid].max = duration
 				self[rid].duration = GetTime() - start
-				self[rid]:SetScript("OnUpdate", function(self, elapsed)
-					self.duration = self.duration + elapsed
-					if self.duration >= self.max or self.duration <= 0 then
-						self.value:SetText("")
-					else
-						self.value:SetText(FormatTime(self.max - self.duration))
-					end
-				end)
+				self[rid]:SetScript("OnUpdate", RuneOnUpdate)
 			end
 		elseif C.classresource == "player" then
 			if event == "NAME_PLATE_UNIT_ADDED" and UnitIsUnit(unit, "player") then
@@ -600,7 +604,8 @@ end
 -- Name / 名字
 local function UpdateName(unitFrame)
 	local name = GetUnitName(unitFrame.displayedUnit, false) or UNKNOWN
-	local level = UnitLevel(unitFrame.unit)
+	local unit = unitFrame.displayedUnit
+	local level = UnitLevel(unit)
 	local hexColor
 	if not C.numberstyle and UnitIsPlayer(unit) and UnitReaction(unit, "player") >= 5 then return end
 	if name then
@@ -622,6 +627,8 @@ local function UpdateName(unitFrame)
 		
 				if level == -1 then
 					unitFrame.name:SetText("|cffff0000??|r "..name)
+				elseif level == UnitLevel("player") and UnitLevel("player") == MAX_PLAYER_LEVEL then
+					unitFrame.name:SetText(name)
 				else
 					unitFrame.name:SetText("|cff"..hexColor..""..level.."|r "..name)
 				end
@@ -683,10 +690,11 @@ end
 local function UpdatePower(unitFrame)
 	local unit = unitFrame.displayedUnit
 	local minPower, maxPower = UnitPower(unit), UnitPowerMax(unit)
-	local perc = minPower/maxPower	
+	local perc = minPower/maxPower
+	local perc_text
 	if minPower and maxPower and maxPower > 0 then
 		perc = minPower/maxPower
-		perc_text = string.format("%d", math.floor(perc*100)) 
+		perc_text = string.format("%d", math.floor(perc*100))
 	else
 		perc = 0
 		perc_text = 0
@@ -863,6 +871,12 @@ local function UpdateSelectionHighlight(unitFrame)
 end
 
 -- special update for mouseover highlight remove / 指向高亮
+local function MouseoverOnUpdate(self, elapsed)
+	if not UnitIsUnit(self.unit, "mouseover") then
+		self.hlmo:Hide()
+	end
+end
+
 local function UpdateMouseover(unitFrame)
 	if not C.HighlightMouseover then return end
 	local unit = unitFrame.unit
@@ -871,12 +885,7 @@ local function UpdateMouseover(unitFrame)
 	else
 		unitFrame.hlmo:Hide()
 	end
-	unitFrame:SetScript("OnUpdate", function(self, elapsed) 
-		local unit = unitFrame.unit
-		if not UnitIsUnit(unit, "mouseover") then
-			unitFrame.hlmo:Hide()
-		end
-	end)
+	unitFrame:SetScript("OnUpdate", MouseoverOnUpdate)
 end
 
 -- Raid mark / 團隊標記
@@ -1282,7 +1291,7 @@ local function OnNamePlateCreated(namePlate)
 		namePlate.UnitFrame.healthBar = CreateFrame("StatusBar", nil, namePlate.UnitFrame)
 		namePlate.UnitFrame.healthBar:SetHeight(8)
 		namePlate.UnitFrame.healthBar:SetPoint("LEFT", 0, 0)
-		namePlate.UnitFrame.healthBar:SetPoint("RIGHT", 0, 8)
+		namePlate.UnitFrame.healthBar:SetPoint("RIGHT", 0, 0)
 		namePlate.UnitFrame.healthBar:SetStatusBarTexture(G.ufbar)
 		namePlate.UnitFrame.healthBar:SetMinMaxValues(0, 1)
 		
@@ -1391,18 +1400,19 @@ local function OnNamePlateCreated(namePlate)
 		namePlate.UnitFrame.hlfocus:SetBlendMode("ADD")
 		namePlate.UnitFrame.hlfocus:Hide()
 		
-		namePlate.UnitFrame.hlfocus = namePlate.UnitFrame:CreateTexture("$parent_Arrow", "BACKGROUND")
-		namePlate.UnitFrame.hlfocus:SetTexture(G.hlglow)
-		namePlate.UnitFrame.hlfocus:SetPoint("BOTTOMLEFT", namePlate.UnitFrame, "LEFT", -10, 4)
-		namePlate.UnitFrame.hlfocus:SetPoint("BOTTOMRIGHT", namePlate.UnitFrame, "RIGHT", 10, 0)
-		namePlate.UnitFrame.hlfocus:SetVertexColor(1, 1, 0)
-		namePlate.UnitFrame.hlfocus:SetTexCoord(0, 1, 1, 0)
-		namePlate.UnitFrame.hlfocus:SetBlendMode("ADD")
-		namePlate.UnitFrame.hlfocus:Hide()
+		namePlate.UnitFrame.hlmo = namePlate.UnitFrame:CreateTexture("$parent_Arrow", "BACKGROUND")
+		namePlate.UnitFrame.hlmo:SetTexture(G.hlglow)
+		namePlate.UnitFrame.hlmo:SetPoint("BOTTOMLEFT", namePlate.UnitFrame, "LEFT", -10, 4)
+		namePlate.UnitFrame.hlmo:SetPoint("BOTTOMRIGHT", namePlate.UnitFrame, "RIGHT", 10, 0)
+		namePlate.UnitFrame.hlmo:SetVertexColor(1, 1, 1)
+		namePlate.UnitFrame.hlmo:SetTexCoord(0, 1, 1, 0)
+		namePlate.UnitFrame.hlmo:SetBlendMode("ADD")
+		namePlate.UnitFrame.hlmo:Hide()
 		
 		namePlate.UnitFrame.icons = CreateFrame("Frame", nil, namePlate.UnitFrame)
-		namePlate.UnitFrame.icons:SetPoint("BOTTOM", namePlate.UnitFrame.name, "TOP", 0, 2)
+		namePlate.UnitFrame.icons:SetPoint("BOTTOM",namePlate.UnitFrame.name, "TOP", 0, -2)
 		namePlate.UnitFrame.icons:SetWidth(140)
+		--namePlate.UnitFrame.icons:SetWidth(namePlate.UnitFrame:GetWidth()) namePlate.UnitFrame.name:GetStringWidth()
 		namePlate.UnitFrame.icons:SetHeight(C.auraiconsize)
 		namePlate.UnitFrame.icons:SetFrameLevel(namePlate.UnitFrame:GetFrameLevel() + 2)
 		
